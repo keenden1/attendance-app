9ee97a315da61f65b8044d61c70554c7153fd00c
Jimmy Bautista <jimmyybautista2000@gmail.com>
2026-05-14 11:59:44 +0800

    sync

--- lib/services/supabase_service.dart
+++ lib/services/supabase_service.dart
@@ -18,6 +18,7 @@ const String kDefaultSupabaseBucket = 'biometrics';
 const String kDefaultApkLink = 'https://mxckrnlamidqqpmdcaks.supabase.co/storage/v1/object/public/biometrics/attendance.apk';
 const String kDefaultWebLink = 'https://attendance-app-omega-green.vercel.app/';
 
+
 class SupabaseService {
   final String url;
   final String key;
